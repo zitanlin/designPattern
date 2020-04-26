414d34c8ade8966e5a180496cdb346aa911a7d8c
zhaohui2 <zhaohui@instra.nsfocus.com>
2020-04-26 16:29:32 +0800

    feat: 增加单例模式简单实现; 优化原 loading 单例

--- src/utils/loading_singleton.js
+++ src/utils/loading_singleton.js
@@ -12,15 +12,47 @@ function getSingletonInstance(fn) {
 }
 
 // 要实现的单例对象
-function showLoadingSingleton(flag = true) {
+function showLoadingSingleton() {
     let divEle = document.createElement("div");
     let loadingImage = document.createElement("img");
     loadingImage.src = "../../../media/image/loading.gif";
     divEle.id = "loading-container";
 
     divEle.appendChild(loadingImage);
-    divEle.style.display = flag ? "flex" : "none";
+    divEle.style.display = "flex";
     document.body.appendChild(divEle);
 
     return divEle;
 }
+
+
+// 下面将创建单例对象、管理单例解耦处理
+var loadingSingletonFactory = (function() {
+    // 创建单例对象
+    function loadingSingleClass() {
+        let divEle = document.createElement("div");
+        let loadingImage = document.createElement("img");
+        loadingImage.src = "../../../media/image/loading.gif";
+        divEle.id = "loading-container";
+
+        divEle.appendChild(loadingImage);
+        divEle.style.display = 'flex';
+        document.body.appendChild(divEle);
+
+        return divEle;
+    }
+
+    var instance;
+
+    return {
+        // 单例管理
+        getInstance: function(flag = true) {
+            if(instance) {
+                instance.style.display = flag ? "flex" : "none";
+            } else {
+                instance = new loadingSingleClass();
+            }
+            return instance;
+        }
+    }
+})();
